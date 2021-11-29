94e07036c38c93973c833c89232349deac0055b3
Francis Tong <francistong@gmail.com>
2021-11-29 17:42:47 +0100

    add CI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scatseisnet",
-    version="0.1.0",
+    version="0.1.3",
     packages=find_packages(),
     python_requires=">=3.6, <4",
     install_requires=["obspy", "parse", "nmmn", "click >= 8"],
