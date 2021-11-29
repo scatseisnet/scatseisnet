b0da6037585d0ca56430f17a1c434b0ba8bf1c54
Francis Tong <francistong@gmail.com>
2021-11-29 15:36:02 +0100

    update setup.py to work as installable package, add compatibility hack with windows file system

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="scatnet",
     version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.6, <4",
+    install_requires=["obspy", "parse", "nmmn", "click >= 8"],
     entry_points={"console_scripts": ["scatnet = scatnet.cli.cli:main",],},
 )
