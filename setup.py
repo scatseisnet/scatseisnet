e60a2556ea26b3e9b611e4c3837614e97eb9c621
Francis Tong <francistong@gmail.com>
2021-11-29 15:43:19 +0100

    rename scatnet to scatseisnet

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scatnet",
+    name="scatseisnet",
     version="0.1.0",
     packages=find_packages(),
     python_requires=">=3.6, <4",
     install_requires=["obspy", "parse", "nmmn", "click >= 8"],
-    entry_points={"console_scripts": ["scatnet = scatnet.cli.cli:main",],},
+    entry_points={"console_scripts": ["scatseisnet = scatseisnet.cli.cli:main",],},
 )
