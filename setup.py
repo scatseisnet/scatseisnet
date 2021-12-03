d8902d77e917fbfc3df9a566c0e2c71a36e5cda1
Francis Tong <francistong@gmail.com>
2021-12-03 14:21:50 +0100

    edit CI to build package and upload to testpypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scatseisnet",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     python_requires=">=3.6, <4",
     install_requires=["astropy", "click", "fastcluster", "nmmn", "numpy", "obspy", "pandas", "parse", "seaborn", "sklearn", "tqdm"],
