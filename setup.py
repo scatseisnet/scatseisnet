663078e9b0fb724b1eb4fe1f39949fca94bdc1c6
Francis Tong <francistong@gmail.com>
2021-12-03 12:52:54 +0100

    update setup.py with new dependencies

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="scatseisnet",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     python_requires=">=3.6, <4",
-    install_requires=["obspy", "parse", "nmmn", "click >= 8", "fastcluster", "cupy"],
+    install_requires=["astropy", "click", "fastcluster", "nmmn", "numpy", "obspy", "pandas", "parse", "seaborn", "sklearn", "tqdm"],
     entry_points={"console_scripts": ["scatseisnet = scatseisnet.cli.cli:main",],},
 )
