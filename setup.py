bf0e2722f2363c67ac2e96731cd18ab3b031dd14
Francis Tong <francistong@gmail.com>
2022-02-23 14:52:30 +0100

    update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scatseisnet",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     python_requires=">=3.6, <4",
     install_requires=["astropy", "click", "fastcluster", "nmmn", "numpy", "obspy", "pandas", "parse", "seaborn", "sklearn", "tqdm"],
