313c99297ef4203588813cf54c1d7cfffc7efd0b
shirleysheep <rene-steinmann@web.de>
2023-03-03 17:15:45 +0100

    include html logo command

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,11 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+
+html_logo = "logo_ssn.png"
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
