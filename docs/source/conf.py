cb9d63b0d6f29c961134025ad45c236a0a25638f
shirleysheep <rene-steinmann@web.de>
2023-03-03 17:56:18 +0100

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
+
 # -- Project information
 
 project = 'Lumache'
